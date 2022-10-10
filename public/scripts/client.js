d61f26d7580fed7bc02e9b9af0e0802372d6afc9
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-10-10 16:46:03 -0400

    Add AJAX get request to load tweets from database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,37 +6,6 @@
 
 $(document).ready(function() {
 
-  
-  // Test / driver code (temporary). Eventually will get this from the server.
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-//
-
-  
   // Function which renders a database of tweets
   const renderTweets = function(tweets) {
     tweets.forEach((x) => {
@@ -45,52 +14,57 @@ $(document).ready(function() {
       $('.previousTweets').append($tweet);
     })
   }
-
+  
   // Function which renders a signle tweet
   const createTweetElement = function (obj) {
     
     let $tweet =  `
-      <article class="tweet">
-
-        <header>
-          <div class="headerLeft">
-            <i class="fa-regular fa-user"></i>
-            <span class="userName">${obj.user.name}</span>
-          </div>
-          <span class="userHandle">${obj.user.handle}</span>
-        </header>
-
-        <span class="userTweet">${obj.content.text}</span>
-        
-        <footer>
-          <span>${obj.created_at}</span>
-          <div class="articleIcons">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>
-
-      </article>
-      `;
-
+    <article class="tweet">
+    
+    <header>
+    <div class="headerLeft">
+    <i class="fa-regular fa-user"></i>
+    <span class="userName">${obj.user.name}</span>
+    </div>
+    <span class="userHandle">${obj.user.handle}</span>
+    </header>
+    
+    <span class="userTweet">${obj.content.text}</span>
+    
+    <footer>
+    <span>${obj.created_at}</span>
+    <div class="articleIcons">
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+    </div>
+    </footer>
+    
+    </article>
+    `;
+    
     return $tweet;
   }
-
-  renderTweets(data);
   
   // Submission handler
-
+  
   $('.new-tweet').submit((event)=>{
     event.preventDefault();
     let urlAsQuery = $('form').serialize();
-  
+    
     $.ajax( {
       url: "/tweets",
-     method: 'POST',
-     data: urlAsQuery
+      method: 'POST',
+      data: urlAsQuery
     })
-    .done(console.log("Post works"));
   });
-
+  
+  // Get database for loading tweets
+  const loadTweets = function() {
+    $.ajax("/tweets",{method: "GET"})
+    .then(function(data) {
+      renderTweets(data);
+    });
+  }
+  loadTweets();
 });
